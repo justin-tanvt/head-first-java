c4c05553d5abf5f014abaecdeeb860943287ed8b
justin-tanvt <justintan28@gmail.com>
2025-01-24 23:18:20 +0800

    [AUTO]: checked in from macbook-air

---
+++ exercises/330pg_11ch/Jukebox3.java
@@ -0,0 +1,15 @@
+import java.util.Collections;
+import java.util.List;
+
+public class Jukebox3 {
+    public static void main(String[] args) {
+        new Jukebox3().go();
+    }
+
+    public void go() {
+        List<SongV3> songList = MockSongs.getSongsV2();
+        System.out.println(songList);
+        Collections.sort(songList);
+        System.out.println(songList);
+    }
+}

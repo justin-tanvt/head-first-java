26b91feba2dbb964f054c54eabcc44d6b1a965f9
justin-tanvt <justintan28@gmail.com>
2025-04-28 16:26:11 +0800

    [AUTO]: checked in from macbook-air

--- exercises/16ch/559pg/WriteAFile.java
+++ exercises/16ch/559pg/WriteAFile.java
@@ -2,8 +2,7 @@ import java.io.FileWriter;
 
 public class WriteAFile {
 	public static void main(String[] args) {
-		try {
-			FileWriter writer = new FileWriter("Foo.txt");
+		try (FileWriter writer = new FileWriter("Foo.txt");) {
 			writer.write("hello foo!\n");
 			writer.close();
 		} catch (Exception e) {

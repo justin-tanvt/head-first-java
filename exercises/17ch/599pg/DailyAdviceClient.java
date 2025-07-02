1f34afb6cc8b434b509ab20553481c46fdb23844
justin-tanvt <justintan28@gmail.com>
2025-07-02 15:46:02 +0800

    [AUTO]: checked in from macbook-air

---
+++ exercises/17ch/599pg/DailyAdviceClient.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.net.InetSocketAddress;
+import java.nio.channels.Channels;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+
+public class DailyAdviceClient {
+  public void go() {
+    InetSocketAddress serverAddress = new InetSocketAddress("127.0.0.1", 5000);
+    try (SocketChannel socketChannel = SocketChannel.open(serverAddress)) {
+      Reader channelReader = Channels.newReader(socketChannel, StandardCharsets.UTF_8);
+      BufferedReader reader = new BufferedReader(channelReader);
+
+      String advice = reader.readLine();
+      System.out.println("Today you should " + advice);
+
+      reader.close();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public static void main(String[] args) {
+    new DailyAdviceClient().go();
+  }
+}

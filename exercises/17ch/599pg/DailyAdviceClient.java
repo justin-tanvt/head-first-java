4f9ed9f8b5ed71b7555570d8a7eb350760710c41
justin-tanvt <justintan28@gmail.com>
2025-09-22 21:08:18 +0800

    [AUTO]: checked in from macbook-air

--- exercises/17ch/599pg/DailyAdviceClient.java
+++ exercises/17ch/599pg/DailyAdviceClient.java
@@ -8,13 +8,13 @@ import java.nio.charset.StandardCharsets;
 
 public class DailyAdviceClient {
   public void go() {
-    InetSocketAddress serverAddress = new InetSocketAddress("127.0.0.1", 5000);
+    InetSocketAddress serverAddress = new InetSocketAddress("localhost", 8080);
     try (SocketChannel socketChannel = SocketChannel.open(serverAddress)) {
       Reader channelReader = Channels.newReader(socketChannel, StandardCharsets.UTF_8);
       BufferedReader reader = new BufferedReader(channelReader);
 
       String advice = reader.readLine();
-      System.out.println("Today you should " + advice);
+      System.out.println("Advice of the day: \"" + advice + "\"");
 
       reader.close();
     } catch (IOException e) {

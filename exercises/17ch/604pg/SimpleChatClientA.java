1c612888b1eb6eb85b7a74eb52f47d862a17118a
justin-tanvt <justintan28@gmail.com>
2025-10-05 17:00:42 +0800

    [AUTO]: checked in from macbook-air

--- exercises/17ch/604pg/SimpleChatClientA.java
+++ exercises/17ch/604pg/SimpleChatClientA.java
@@ -1,38 +1,86 @@
 import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+import java.awt.BorderLayout;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.nio.channels.Channels;
 import java.nio.channels.SocketChannel;
+import java.time.LocalDateTime;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class SimpleChatClientA {
 
-	private JTextField outgoing;
-	private PrintWriter writer;
+  private JTextField outgoing;
+  private SocketChannel channel;
+  private PrintWriter writer;
 
-	public static void main(String[] args) {
-		new SimpleChatClientA().go();
-	}
+  private static final String GUI_WINDOW_NAME = "Ludicrously Simple Chat Client";
+  private static final int GUI_WINDOW_LENGTH = 400;
+  private static final int GUI_WINDOW_HEIGHT = 100;
+  private static final int GUI_TEXTFIELD_LENGTH = 20;
+  private static final String GUI_BUTTON_TEXT = "Send";
 
-	public void go() {
-		System.out.println("in go() method!");
-		setUpNetworking();
-	}
+  public static void main(String[] args) {
+    System.out.println(LocalDateTime.now() + " > " + "SimpleChatClientA" + "[" + Thread.currentThread().getName() + "]" + " -- " + "main()");
+    new SimpleChatClientA().go();
+  }
 
-	private void setUpNetworking() {
-		System.out.println("in setUpNetworking() method!");
-		InetSocketAddress serverAddress = new InetSocketAddress(
-				SimpleChatConstants.IP_ADDRESS, SimpleChatConstants.PORT_NUMBER);
-		try (SocketChannel channel = SocketChannel.open(serverAddress)) {
-			writer = new PrintWriter(Channels.newWriter(channel, UTF_8));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+  public void go() {
+    debug("go()");
+    setUpNetworking();
+    setUpGui();
+  }
 
-	private void sendMessage() {
+  private void setUpNetworking() {
+    debug("setUpNetworking()");
+    InetSocketAddress serverAddress = new InetSocketAddress(
+        SimpleChatConstants.IP_ADDRESS, SimpleChatConstants.PORT_NUMBER);
+    debug("trying to open socket channel next");
+    try {
+      channel = SocketChannel.open(serverAddress);
+      debug("opened socket channel");
+      writer = new PrintWriter(Channels.newWriter(channel, UTF_8));
+      debug("created writer");
+    } catch (IOException e) {
+      e.printStackTrace();
+      throw new RuntimeException("failed to set up networking!");
+    }
+  }
 
-	}
+  private void setUpGui() {
+    debug("setUpGui()");
+    outgoing = new JTextField(GUI_TEXTFIELD_LENGTH);
+
+    JButton sendButton = new JButton(GUI_BUTTON_TEXT);
+    sendButton.addActionListener(e -> sendMessage());
+
+    JPanel panel = new JPanel();
+    panel.add(outgoing);
+    panel.add(sendButton);
+
+    JFrame frame = new JFrame(GUI_WINDOW_NAME);
+    frame.getContentPane().add(BorderLayout.CENTER, panel);
+    frame.setSize(GUI_WINDOW_LENGTH, GUI_WINDOW_HEIGHT);
+    frame.setVisible(true);
+    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+  }
+
+  private void sendMessage() {
+    debug("sendMessage()");
+    writer.println(outgoing.getText());
+    debug("sent message to server");
+    writer.flush();
+    outgoing.setText("");
+    debug("reset text field");
+    outgoing.requestFocus();
+  }
+
+  private void debug(String message) {
+    System.out.println(LocalDateTime.now() + " > " + this.getClass().getName() + "[" + Thread.currentThread().getName() + "]" + " -- " + message);
+  }
 }
---
+++ exercises/17ch/604pg/SimpleChatServer.java
@@ -0,0 +1,91 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.nio.channels.Channels;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class SimpleChatServer {
+  private final List<PrintWriter> clientWriters = new ArrayList<>();
+
+  public static void main(String[] args) {
+    System.out.println(LocalDateTime.now() + " > " + "SimpleChatServer" + "[" + Thread.currentThread().getName() + "]" + " -- " + "main()");
+    new SimpleChatServer().go();
+  }
+
+  private void go() {
+    debug("in go()");
+    ExecutorService threadPool = Executors.newCachedThreadPool();
+    debug("created cached thread pool");
+    try (ServerSocketChannel serverSocketChannel = ServerSocketChannel.open()) {
+      debug("opened server socket channel");
+      serverSocketChannel.bind(new InetSocketAddress(SimpleChatConstants.PORT_NUMBER));
+      debug("bound server socket channel to port");
+
+      while (serverSocketChannel.isOpen()) {
+        debug("waiting to accept client socket");
+        SocketChannel clientSocket = serverSocketChannel.accept();
+        debug("accepted client socket");
+        clientWriters.add(new PrintWriter(Channels.newWriter(clientSocket, UTF_8)));
+        debug("created client writer");
+        threadPool.submit(new ClientHandler(clientSocket));
+        debug("created client handler and added to thread pool");
+        debug("got a connection");
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void tellEveryone(String message) {
+    debug("in tellEveryone()");
+    for (PrintWriter writer : clientWriters) {
+      writer.println(message);
+      writer.flush();
+    }
+  }
+
+  private void debug(String message) {
+    System.out.println(LocalDateTime.now() + " > " + this.getClass().getName() + "[" + Thread.currentThread().getName() + "]" + " -- " + message);
+  }
+
+  public class ClientHandler implements Runnable {
+    BufferedReader reader;
+    SocketChannel socket;
+
+    public ClientHandler(SocketChannel clientSocket) {
+      debug("in constructor");
+      socket = clientSocket;
+      reader = new BufferedReader(Channels.newReader(socket, UTF_8));
+    }
+
+    public void run() {
+      debug("in run()");
+      String message;
+      debug("going to start reading messages from reader in a while-loop");
+      try {
+        while ((message = reader.readLine()) != null) {
+          debug("received non-null message from reader");
+          System.out.println("received from client : " + message);
+          tellEveryone(message);
+          debug("broadcasted message to clients");
+        }
+        debug("exited reader while-loop");
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+
+    private void debug(String message) {
+      System.out.println(LocalDateTime.now() + " > " + this.getClass().getName() + "[" + Thread.currentThread().getName() + "]" + " -- " + message);
+    }
+  }
+}
